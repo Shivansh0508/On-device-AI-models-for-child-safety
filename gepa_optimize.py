289842aa7fab42d5ea6ba6b2b00456a1471292ae
SHIVANSH SAXENA <f20230637@pilani.bits-pilani.ac.in>
2026-05-28 09:32:33 +0530

    Update gepa_optimize.py

--- gepa_optimize.py
+++ gepa_optimize.py
@@ -11,3 +11,28 @@ import json
 import re
 from tqdm import tqdm
 import getpass
+
+# CONFIG# 
+SEED = 42
+random.seed(SEED)
+np.random.seed(SEED)
+
+# OPENROUTER SETUP
+os.environ["OPENROUTER_API_KEY"] = getpass.getpass("Enter OpenRouter API key: ")
+
+# Task model 
+task_lm = dspy.LM(
+    model="openrouter/google/gemma-3-27b-it",
+    temperature=0.1,
+    max_tokens=128,
+    api_key=os.environ["OPENROUTER_API_KEY"],
+)
+
+# Reflection model 
+prompt_lm = dspy.LM(
+    model="openrouter/meta-llama/llama-3.3-70b-instruct",
+    temperature=0.7,
+    max_tokens=2048,
+    api_key=os.environ["OPENROUTER_API_KEY"],
+)
+dspy.configure(lm=task_lm)
