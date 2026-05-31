024837e3f92a24466e2ff73b092a20312e9c5aa1
SHIVANSH SAXENA <f20230637@pilani.bits-pilani.ac.in>
2026-05-31 11:39:24 +0530

    Update translate_multilingual.py

--- translate_multilingual.py
+++ translate_multilingual.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
+import requests
+import os
+import json
+import re
+import time
+from tqdm import tqdm
+from glob import glob
+from sklearn.metrics import f1_score
+import getpass
 
